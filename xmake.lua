ec36e7b0338f60fe641808aadea73180bf5e5ed2
Saikari <lin@sz.cn.eu.org>
2026-04-22 02:47:35 +0300

    Bump version from 0.0.12 to 0.0.13

--- xmake.lua
+++ xmake.lua
@@ -8,7 +8,7 @@ add_rules("plugin.compile_commands.autoupdate", {outputdir = ".vscode", lsp = "c
 set_languages("c++20")
 
 -- CI_PROJECT_VERSION
-set_version("0.0.12")
+set_version("0.0.13")
 
 -- Download aui package to use for targets later
 add_requires("aui 7.1.2")
